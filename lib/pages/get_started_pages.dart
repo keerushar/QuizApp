9a20a6321effd399cb9c686f31761d653f517000
keerushar <kiran.sharma@gurzu.com>
2024-06-14 06:53:59 +0545

    Setup layout for get started

--- lib/pages/get_started_pages.dart
+++ lib/pages/get_started_pages.dart
@@ -6,11 +6,19 @@ class GetStartedPages extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        children: [
-          Image.asset("assets/images/get_started.png")
-        ],
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Image.asset("assets/images/get_started.png"),
+            ),
+            Text("Welcome to quiz app"),
         
+            ElevatedButton(onPressed: (){}, child: Text("Let's get started"))
+          ],
+          
+        ),
       ),
     );
   }

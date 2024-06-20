852f331a21f77240cd803e627a7dd262472aa2da
keerushar <kiran.sharma@gurzu.com>
2024-06-20 06:53:54 +0545

    Setup validator

--- lib/pages/enter_name_page.dart
+++ lib/pages/enter_name_page.dart
@@ -1,40 +1,52 @@
 import 'package:flutter/material.dart';
+import 'package:quiz_app/pages/quiz_play_page.dart';
 
 class EnterNamePage extends StatelessWidget {
   const EnterNamePage({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final formKey = GlobalKey<FormState>();
     TextEditingController? inputNameController;
 
     return Scaffold(
       body: SingleChildScrollView(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-
-          children: [
-            
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Image.asset("assets/images/get_started.png"),
-            ),
-
-            Text("Enter your name to get start"),
-            TextField(
-              cursorColor: Theme.of(context).primaryColor,
-              controller: inputNameController,
-              decoration: const InputDecoration(
-                hintStyle: TextStyle(color: Colors.grey),
-                hintText: "Enter your name",
-                prefixIcon: Icon(Icons.person),
-                contentPadding: EdgeInsets.only(top: 14),
+        child: Form(
+          key: formKey,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Image.asset("assets/images/get_started.png"),
               ),
-            ),
-
-            ElevatedButton(onPressed: (){
-              
-            }, child: Text("Let's play"))
-          ],
+              Text("Enter your name to get start"),
+              TextFormField(
+                cursorColor: Theme.of(context).primaryColor,
+                controller: inputNameController,
+                decoration: const InputDecoration(
+                  hintStyle: TextStyle(color: Colors.grey),
+                  hintText: "Enter your name",
+                  prefixIcon: Icon(Icons.person),
+                  contentPadding: EdgeInsets.only(top: 14),
+                ),
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Please enter your name';
+                  }
+                  return null;
+                },
+              ),
+              ElevatedButton(
+                  onPressed: () {
+                    if (formKey.currentState!.validate()) {
+                      Navigator.of(context).push(
+                          MaterialPageRoute(builder: (_) => const QuizPlayPage()));
+                    }
+                  },
+                  child: Text("Let's play"))
+            ],
+          ),
         ),
       ),
     );
